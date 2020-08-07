51d92bb3209a466c4a2dafbe14041098215edf6f
Abdullah <abdullah.ali@cyber.gc.ca>
2020-08-07 10:07:22 -0400

    ratdecoders

--- configextractor.py
+++ configextractor.py
@@ -89,7 +89,6 @@ class ConfigExtractor(ServiceBase):
         result = Result()
         # Run Ratdecoders
         output = cli.run_ratdecoders(request.file_path, self.mwcp_reporter)
-  X
 
         if type(output) is dict:
             for parser, fields in output.items():
