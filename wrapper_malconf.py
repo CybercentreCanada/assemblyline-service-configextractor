d841c19e0de0718167cd7fa2b899acd595b3c9ad
Abdullah <abdullah.ali@cyber.gc.ca>
2021-03-24 12:19:36 -0400

    change print to logs while running in service and as cli

--- wrapper_malconf.py
+++ wrapper_malconf.py
@@ -6,17 +6,27 @@ RAT_DECODERS_PATH = os.path.join(ROOT_DIR, "RATDecoders")
 sys.path.insert(0, RAT_DECODERS_PATH)
 from malwareconfig import fileparser
 from malwareconfig.modules import __decoders__, __preprocessors__
+import logging
+
+log_level = os.getenv('LOG_LEVEL') or 'INFO'
+handler = logging.StreamHandler(sys.stdout)
+logger = logging.getLogger('assemblyline.svc.configextractor.wrapper_malconf')
+
+if sys.stdin.isatty():
+    #if running as cli then redirect logs to stdout
+    logger.addHandler(handler)
+    logger.setLevel(log_level)
 
 
 def preprocess(file_path):
     # Open and parse the file
-    print("[+] Loading File: {0}".format(file_path))
+    logger.info("[+] Loading File: {0}".format(file_path))
     file_info = fileparser.FileParser(file_path=file_path)
-    print("  [-] Found: {0}".format(file_info.malware_name))
+    logger.info("  [-] Found: {0}".format(file_info.malware_name))
     # First we preprocesss
     # Check for a packer we can unpack
     if file_info.malware_name in __preprocessors__:
-        print("  [+] Running PreProcessor {0}".format(file_info.malware_name))
+        logger.info("  [+] Running PreProcessor {0}".format(file_info.malware_name))
         module = __preprocessors__[file_info.malware_name]['obj']()
         module.set_file(file_info)
         module.pre_process()
@@ -26,14 +36,14 @@ def preprocess(file_path):
 
 def process_file(file_info):
     if file_info.malware_name in __decoders__:
-        print("  [-] Running Decoder")
+        logger.info("  [-] Running Decoder")
         module = __decoders__[file_info.malware_name]['obj']()
         module.set_file(file_info)
         module.get_config()
         conf = module.config
-        print("  [-] Config Output\n")
+        logger.info("  [-] Config Output\n")
         json_config = json.dumps(conf, indent=4, sort_keys=True)
-        print(json_config)
+        logger.info(json_config)
         return conf
     else:
 
@@ -41,34 +51,34 @@ def process_file(file_info):
 
 
 def list_decoders():
-    print("[+] Listing Decoders")
+    logger.info("[+] Listing Decoders")
     for name in __decoders__.keys():
-        print("  [-] Loaded: {0}".format(name))
+        logger.info("  [-] Loaded: {0}".format(name))
 
-    print("[+] Listing PreProcessors")
+    logger.info("[+] Listing PreProcessors")
     for name in __preprocessors__.keys():
-        print("  [-] Loaded: {0}".format(name))
+        logger.info("  [-] Loaded: {0}".format(name))
     sys.exit()
 
 
 def check_file(f_path=None):
     # We need at least one arg
     if f_path == None:
-        print("[!] Not enough Arguments, Need at least file path\n")
+        logger.info("[!] Not enough Arguments, Need at least file path\n")
         sys.exit()
     # Check for file or dir
     is_file = os.path.isfile(f_path)
     is_dir = os.path.isdir(f_path)
     if is_dir:
-        print("[!] Path is directory not a file.\n")
+        logger.info("[!] Path is directory not a file.\n")
         sys.exit()
     if not is_file:
-        print("[!] You did not provide a valid file.\n")
+        logger.info("[!] You did not provide a valid file.\n")
         sys.exit()
 
 
 if __name__ == "__main__":
-    print("[+] RATDecoders Running")
+    logger.info("[+] RATDecoders Running")
     path = sys.argv[1]
     check_file(path)
     file = preprocess(path)
